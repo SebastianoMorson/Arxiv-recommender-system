080986987348f54797a069ad3db05f10a4d9cd2a
BrainNotBraining <114572640+enricotazzer@users.noreply.github.com>
2025-06-04 13:55:48 +0200

    Add files via upload
    reccomendation 2.0

--- recommender.py
+++ recommender.py
@@ -36,12 +36,18 @@ def load_data(ds_file_path, embeddings_file_path):
     print('Dataset loaded successfully.')
     return df_emb
 
-df = load_data('/Users/enricotazzer/Desktop/hackathon/data/arxiv_dataset.csv', '/Users/enricotazzer/Desktop/hackathon/data/arxiv_specter_embeddings.csv') # Replace with actual file paths
+#df = load_data('/Users/enricotazzer/Desktop/hackathon/data/arxiv_dataset.csv', '/Users/enricotazzer/Desktop/hackathon/data/arxiv_specter_embeddings.csv') # Replace with actual file paths
 
 def user_interest_filtering(user_interests):
     df_user = df[df['categories'].apply(lambda x: isinstance(x, str) and any(cat in x.split() for cat in user_interests))]
     return df_user
 
+def random_not_user_interests(df, user_interests, n=20):
+    mask = df['categories'].apply(
+        lambda x: isinstance(x, str) and not any(cat in x.split() for cat in user_interests)
+    )
+    df_not_interests = df[mask]
+    return df_not_interests.sample(n=min(n, len(df_not_interests)), random_state=42)
 
 def extract_samples(user_interests, n_samples=2):
     samples = []
@@ -52,26 +58,21 @@ def extract_samples(user_interests, n_samples=2):
 
 
 def knn_recommender(user_interests):
+    if not isinstance(user_interests, list): ## concorrenza attendere file registration.json
+        raise ValueError("user_interests must be a list of categories.")
+    
     df_user = user_interest_filtering(user_interests)
-    guess_samples = extract_samples(user_interests, n_samples=2)
-    guess_samples = guess_samples[['id', 'categories']].drop_duplicates()
     embedding_cols = [col for col in df_user.columns if col not in ['id', 'categories', 'title']]
     embeddings = df_user[embedding_cols].values
     y = df_user.index
-
-    nn = KNeighborsClassifier(n_neighbors=20, metric='cosine', n_jobs=-1)
+    nn = KNeighborsClassifier(n_neighbors=100, metric='cosine', n_jobs=-1)
     nn.fit(embeddings, y)
-
-    for _ in range(5):
-        
-        idx = random.randint(0, len(y)-1)
-        sample_title = df_user.iloc[idx]["id"] if "title" not in df_user.columns else df_user.iloc[idx]["title"]
-        dist, index = nn.kneighbors(X=embeddings[idx, :].reshape(1, -1))
-        print(f"Sample:\n{sample_title}\n")
-        for i in range(1, 20):
-            rec_idx = index[0][i]
-            rec_title = df_user.iloc[rec_idx]["id"] if "title" not in df_user.columns else df_user.iloc[rec_idx]["title"]
-            print(f"Recommendation {i}:\n{rec_title}\n")
-        print("===============\n")
-
+    idxs = [np.random.randint(0, len(y)-1) for _ in range(10)]  # Randomly select 10 indices from the user dataset
+    mean_embedding = np.mean(embeddings[idxs, :], axis=0)
+    idx = random.randint(0, len(y)-1) # Randomly select an index from the user dataset
+    dist, index = nn.kneighbors(X=mean_embedding.reshape(1, -1))
+    best_reccomendation = index[:70][0]
+    worst_reccomendation = index[-10:][0]
+    random_recommendation = random_not_user_interests(df_emb, user_interests)
+    recommendations = pd.concat([df_user.iloc[best_reccomendation], df_user.iloc[worst_reccomendation], random_recommendation])
 
